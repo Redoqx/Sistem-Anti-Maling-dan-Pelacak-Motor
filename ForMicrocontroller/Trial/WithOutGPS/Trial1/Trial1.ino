f3dbe35dd11027ec8b19d3302dee6f8ad6e4eaf6
Redoqx <76461626+Redoqx@users.noreply.github.com>
2021-12-27 16:57:05 +0700

    Yup

--- ForMicrocontroller/Trial/WithOutGPS/Trial1/Trial1.ino
+++ ForMicrocontroller/Trial/WithOutGPS/Trial1/Trial1.ino
@@ -1,6 +1,8 @@
 #include <TinyGPS++.h>
 #define RXD2 16
 #define TXD2 17
+#define Sein 14
+#define Klakson 15
 HardwareSerial neogps(1);
 
 TinyGPSPlus gps;
@@ -9,6 +11,27 @@ void setup() {
   Serial.begin(115200);
   neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
   delay(2000);
+  pinMode(Sein, OUTPUT);
+  pinMode(Klakson, Output);
+  digitalWrite(Sein, LOW);
+  digitalWrite(Klakson, LOW);
+}
+
+boolean Pencurian(boolean newData){
+    if(newData) if(gps.speed.kmph() > 10.0)
+    return true;
+    else false;
+}
+
+void Alarm(){
+    while(true){//replace with NFC card recognized
+        digitalWrite(Sein, HIGH);
+        digitalWrite(Klakson, HIGH);
+        delay(1000);
+        digitalWrite(Sein, LOW);
+        digitalWrite(Klakson, LOW);
+        delay(1000);
+    }
 }
 
 void loop() {
@@ -35,5 +58,8 @@ void loop() {
   }
   else{
     Serial.println("No Data"); 
+  }
+  if(Pencurian(newData)){
+
   }
 }
---
+++ ForMicrocontroller/Yeah_v0.5/Yeah_v0.5.ino
@@ -0,0 +1,135 @@
+//RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN 22
+#define SS_PIN 21
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+//UserIdentify
+String UID, User = "No data!";
+bool giveAcc;
+#define RST_User 27 //Ganti user
+#define Curr_eng 14 //cek kondisi mesin (hidup/koid?)
+#define Acc_eng 12 //beri akses mesin
+
+//GPS
+#include <TinyGPS++.h>
+#define RXD2 16
+#define TXD2 17
+HardwareSerial neogps(1);
+TinyGPSPlus gps;
+//Blynk
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <BlynkSimpleEsp32_SSL.h>
+char auth[] = "mCxqHTioIfPdmzwvaLYa4M9sSm8QN1Gz";
+char ssid[] = "Laras";
+char pass[] = "29031969";
+//char ssid[] = "RED";
+//char pass[] = "namakamu";
+WidgetMap myMap(V1);
+float lat;
+float lon;  
+//alarm
+#define alarm 26
+//confirm
+#define confirm 2
+
+void setup(){
+  Serial.begin(115200);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
+  Serial.println(F("RFID Ready"));
+
+  pinMode(RST_User, INPUT);
+  pinMode(Curr_eng, INPUT);
+  pinMode(Acc_eng, OUTPUT);
+  pinMode(alarm, OUTPUT);
+  
+  pinMode(confirm, OUTPUT);
+  digitalWrite(Acc_eng, LOW);
+  giveAcc = 0;  
+
+  //GPS
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  delay(2000);
+
+  //Blynk  
+  Blynk.begin(auth, ssid, pass);
+  myMap.clear();
+}
+
+void getUID(){
+  if(!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) return;
+  UID="";
+  Serial.print(F("Card UID: "));
+  for(byte i=0; i<mfrc522.uid.size; i++)
+  UID+=(mfrc522.uid.uidByte[i]<0x10 ? "0" : "") +
+  String(mfrc522.uid.uidByte[i], HEX) +
+  (i != (mfrc522.uid.size -1) ? ":" : "");
+  UID.toUpperCase();
+  Serial.println(UID);
+  mfrc522.PICC_HaltA();
+  delay(10);
+}
+void gantiKartu(){
+  getUID();
+  User = UID;
+  Serial.println(F("User card has changed to this UID"));
+}
+bool userConfirmed(){
+  getUID();
+  return (User == UID);
+}
+
+void loop(){
+  //ganti kartu
+  if(digitalRead(RST_User)) gantiKartu();
+  //mengidentifiasi pengguna
+  if(userConfirmed()){
+    digitalWrite(Acc_eng, HIGH);
+    Serial.println(F("Silahkan nyalakan mesin"));
+    UID = "";
+    delay(5000);
+  }
+  //mengcek kondisi mesin
+  if(!digitalRead(Curr_eng)){
+    digitalWrite(Acc_eng, LOW);
+    Serial.println(F("Akses ditutup!"));
+  }
+  //mengecek kemalingan
+  //matikan alarm jika
+
+  //GPS
+  boolean newData = false;
+  for(unsigned long start = millis();millis()-start<1000;){
+    while(neogps.available()){
+      if(gps.encode(neogps.read())){
+        newData = true;
+      }
+    }
+  }
+
+  if(newData){
+    newData = false;
+    lat = gps.location.lat();
+    lon = gps.location.lng();
+    Serial.println(gps.satellites.value());
+    Serial.print(F("Lat: "));
+    Serial.println(lat,6);
+    Serial.print(F("Lng: "));
+    Serial.println(lon,6);
+    Serial.print(F("Speed: "));
+    Serial.println(gps.speed.kmph());
+    Serial.print(F("ALT: "));
+    Serial.println(gps.altitude.meters());
+    int index = 0;
+    myMap.location(index, lat, lon, "value");
+  }
+  else{
+    Serial.println("No Data"); 
+  }
+  Blynk.run();
+}
